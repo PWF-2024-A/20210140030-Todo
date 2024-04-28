d3988a4e9de113f36517c10aaa11fd0387fadd06
Fahma Rosyidah <fahmarosyi@gmail.com>
2024-04-28 21:36:56 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
-
+});
+Route::middleware('admin')->group(function () {
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
